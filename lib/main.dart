8b260adb415b6c0833402fbf643a2a57676413f4
Huakhanhdoan <21020759@vnu.edu.vn>
2024-03-06 20:03:27 +0700

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Banana'),
+      home: const MyHomePage(title: 'ITKIT'),
     );
   }
 }
